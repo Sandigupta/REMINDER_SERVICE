2ea29ee113c199abe21922c5db4494a49045f505
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-03-15 16:27:27 +0530

    First commit seting up reminder service

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const {PORT}=require('./config/serverConfig')
+
+const setupAndStartServer = () => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(PORT, () => {
+    console.log("Server is running on port",$(PORT));
+})
+
+}
+
+setupAndStartServer();
