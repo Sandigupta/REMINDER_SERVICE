ebf4d9cbfed17035513a85802e7b11c8673270e9
Sandigupta <sandeep.2022ug1100@iiitranchi.ac.in>
2025-03-23 03:17:28 +0530

    Setup the RabbitMQ logic int the ReminderService

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ const { PORT } = require('./config/serverConfig')
 const {sendBasicEmail}=require("./service/emailService")
 
 const jobs = require('./utils/job')
+const {createChannel} = require('./utils/messageQueue');
 
 const TicketController = require('./controller/ticket-controller');
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
 // app.use(bodyParser.json());
     // app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.json()); // Ensures JSON requests are parsed
     app.use(express.urlencoded({ extended: true })); // Handles URL-encoded data
 
-    
+    const channel = await createChannel();
     app.post('/api/v1/tickets', TicketController.create);
 
 app.listen(PORT, () => {
